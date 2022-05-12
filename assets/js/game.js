83fed3e67299dfa0d9c574386ae1f21ffabd8551
Elena Carey <theelenacarey@gmail.com>
2022-05-12 17:15:40 -0500

    made new robots appear for rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,8 +20,14 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
+
+    // repeat and execute as long as the enemy-robot is alive 
+    while(enemyHealth > 0) {
+      // place fight function code block here . . .
+    
+
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  //window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -66,14 +72,17 @@ var fight = function(enemyName) {
     }
     // if no (false), ask question again by running fight() again
     else {
-      for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
-}
+      for (var i = 0; i < enemyNames.length; i++) {
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
     }
     // if player did not chose 1 or 2 in prompt
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 }; // end of fight function
 
 // run fight function to start game
